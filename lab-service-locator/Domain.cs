c8088f2a4a3fcfd31918e6eccf2dfa74c1ec4514
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2023-09-07 11:49:57 -0300

    Adicionar fontes

---
+++ lab-service-locator/Domain.cs
@@ -0,0 +1,28 @@
+﻿namespace lab_service_locator
+{
+    public class Domain
+    {
+        public String? Nome { get; set; }
+    }
+
+    public interface IServico
+    {
+        public void facaAlgo(Domain domain);
+    }
+
+    public class Servico : IServico
+    {
+        public void facaAlgo(Domain domain)
+        {
+            Console.WriteLine("Serviço A: " + domain.Nome);
+        }
+    }
+
+    public class ServicoB : IServico
+    {
+        public void facaAlgo(Domain domain)
+        {
+            Console.WriteLine("Serviço B: " + domain.Nome);
+        }
+    }
+}
